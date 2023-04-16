7bc09a26cc96326ea7238c0dc8c8c7f37ee3413e
jren303 <jren303@gatech.edu>
2023-04-16 14:47:12 -0700

    frontendcode

---
+++ app.py
@@ -0,0 +1,60 @@
+#Import main library
+import numpy as np
+from sklearn.preprocessing import StandardScaler, normalize
+#Import Flask modules
+from flask import Flask, request, render_template, jsonify
+import pandas as pd
+#Import pickle to save our regression model
+import pickle 
+
+model_d = {'silverado 1500':24761, 'tacoma':26359,  'a5':7614, 'colorado': 10995, 'odyssey': 20912, 'civic': 10586, 'nx': 20868, 'rx': 23321, 'x5': 28932, '320': 2614, 'q5': 22140, 'c300': 9548, 'countryman': 11509, 'camery': 9803, 'mazda3': 19809}
+manufactuer_d = {'audi': 3, 'bmw': 4, 'chevrolet': 7, 'ford': 13,  'honda': 16, 'lexus': 23, 'mazda': 25, 'mercedes-benz': 26, 'mini': 28, 'toyota': 40}
+drive_d = {'4wd': 0, 'fwd': 1, 'nan': 2, 'rwd': 3}
+fuel_d = {'diesel': 0, 'electric': 1, 'gas': 2, 'hybrid': 3, 'nan': 4, 'other': 5}
+
+
+#Initialize Flask and set the template folder to "static"
+app = Flask(__name__, template_folder='', static_folder='static')
+
+#Open our model 
+#model = pickle.load(open('voting_model.pkl','rb'))
+
+#create our "home" route using the "index.html" page
+@app.route('/', methods = ['GET'])
+def home():
+    return render_template('index.html')
+
+#Set a post method to yield predictions on page
+@app.route('/', methods = ['POST'])
+def estimate():
+     #obtain all form values and place them in an array, convert into integers
+    #int_features = [int(x) for x in request.form.values()]
+    #Combine them all into a final numpy array
+    #final_features = [np.array(int_features)]
+    #predict the price given the values inputted by user
+    
+
+    inputManufactuer = request.form['inputManufactuer']
+    inputYear = request.form['inputYear']
+    inputModel = request.form['inputModel']
+    inputDrive = request.form['inputDrive']
+    inputOdometer = request.form['inputOdometer']
+    inputFuel = request.form['inputFuel']
+
+    features = [[inputYear, inputOdometer, model_d[inputModel], drive_d[inputDrive], fuel_d[inputFuel], manufactuer_d[inputManufactuer]]]
+    df = pd.DataFrame(features, columns=['year','odometer','model','drive','fuel','manufacturer'])
+    scaler = StandardScaler()
+    final_features = pd.DataFrame(scaler.fit_transform(df), columns = df.columns)
+    #prediction = model.predict(final_features)
+    #prediction = '50'
+    #print(prediction)
+
+    #Round the output to 2 decimal places
+    #output = round(prediction[0], 2)
+    output = '50'
+    #print(request.form)
+    return render_template('index.html', inputManufactuer=inputManufactuer, inputYear=inputYear, inputModel=inputModel, inputDrive=inputDrive, inputOdometer=inputOdometer, inputFuel=inputFuel, price=output)
+
+#Run app
+if __name__ == "__main__":
+    app.run(debug=True)
